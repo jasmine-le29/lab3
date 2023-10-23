afba03737e00663c4b30d798d4ad1b921fb9bf16
jasmine-le29 <jasmineple@yahoo.com>
2023-10-23 13:50:45 -0700

    update lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,9 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
-    ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    int[] input1 = { 1, 2, 3 };
+    assertArrayEquals(new int[]{ 3, 2, 1 },
+    ArrayExamples.reversed(input1));
 	}
 
 
